9d3fcdcc502e5cf0d59f77fa758b63bc582c30fe
mdshaedhossen <shaheddiu23@gmail.com>
2022-05-07 00:32:18 +0600

    login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Header from './Shared/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Counter from '../Counter/Counter';
 import Items from '../Items/Items';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div id='home'>
             <Banner></Banner>
             <Items></Items>
+            {/* <Counter></Counter> */}
         </div>
     );
 };
